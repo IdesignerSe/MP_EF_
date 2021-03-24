f27ec2d6181c15dc979afea33adc6b5e23f55be3
idesignerse <heberandrade@gmail.com>
2021-03-24 10:41:31 +0100

    Starter
    Dummy starter Hello world

---
+++ MP_EF_HeberAndrade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MP_EF_HeberAndrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
