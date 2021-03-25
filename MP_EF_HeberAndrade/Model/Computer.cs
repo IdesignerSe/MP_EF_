8481985b8b607c189e6541119192034cb5581210
idesignerse <heberandrade@gmail.com>
2021-03-25 21:00:43 +0100

    Update
    Update

--- MP_EF_HeberAndrade/Model/Computer.cs
+++ MP_EF_HeberAndrade/Model/Computer.cs
@@ -3,10 +3,7 @@
 {
     public class Computer : Asset
     {
-        public Computer(string v1, string v2, int v3, int v4)
-        {
-        }
-
+ 
         public Computer(string brand, string modelName, int purchaseDate, int inicialCost, int expiredDate, int expiredCost)
         {
             Brand = brand;
--- MP_EF_HeberAndrade/Program.cs
+++ MP_EF_HeberAndrade/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace MP_EF_HeberAndrade
 {
@@ -9,46 +10,107 @@ namespace MP_EF_HeberAndrade
     {
         static void Main(string[] args)
         {
-            
             using (var dbContext = new AssetsContext())
             {
 
-                 //(string brand, string modelName, int purchaseDate, int inicialCost, int expiredDate, int expiredCost)
+                Computer computerItem = new Computer("MacBook", "2018 15 inch ", 20180101, 13000, 20211201, 8000);
+                string classBrand = computerItem.GetType().Name;
+                PropertyInfo brand = computerItem.GetType().GetProperty("Brand");
+                List<PropertyInfo> properties = computerItem.GetType().GetProperties().ToList();
+
+
+                //foreach(PropertyInfo property in properties)
+                //{
+                //    Console.Write(property.Name.PadRight(property.Name.Length + 7));
+                //}
+
+                //Console.WriteLine();
+
+                //foreach (PropertyInfo property in properties)
+                //{
+                //        Console.Write(property.GetValue(computerItem).ToString().PadRight(property.GetValue(computerItem).ToString().Length + 7));
+                //}
+
+                Computer computerItem2 = new Computer("MacBook", "2019 15 inch ", 20180101, 16000, 20211201, 10000);
+                List<Computer> computers = new List<Computer>();
+
+                computers.Add(computerItem);
+                computers.Add(computerItem2);
+    //            computers = AssetsContext.Computers.ToList();
+
+                Dictionary<string, int> colWidths = new Dictionary<string, int>();
+
+                foreach (PropertyInfo property in properties)
+                {
+                    int colLength = property.Name.Length;
+                    int tmpLength = 0;
+                    int dataLength = 0;
+
+                    foreach (Computer computer in computers)
+                    {
+                        tmpLength = computers.Max(computer => computer.GetType().GetProperty(property.Name)).GetValue(computer).ToString().Length;
+                        dataLength = Math.Max(dataLength, tmpLength);
+                    }
+                    int maxLength = Math.Max(colLength, dataLength);
+
+                    colWidths.Add(property.Name, maxLength);
+                }
+
+                Console.WriteLine();
+
+                //Print Header with correct widths
+                foreach (var colWidth in colWidths)
+                {
+                    Console.Write(colWidth.Key.PadRight(colWidth.Value + 2));
+                }
+
+                Console.WriteLine();
+
+                //Print Data with correct widths
+                foreach (Computer computer in computers)
+                {
+                    foreach (var colWidth in colWidths)
+                    {
+                        var c = computer.GetType().GetProperties().Where(c => c.Name == colWidth.Key).FirstOrDefault();
+                        Console.Write(c.GetValue(computer).ToString().PadRight(colWidth.Value + 2));
+                    }
+                    Console.WriteLine();
+
+                }
+
+
+
+
+
+                //Computer computer1 = new Computer("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000);
+
+                //dbContext.Add(computer1);
+                //dbContext.SaveChanges();
+
+
+                //Computer computer2 = new Computer("MacBook", "Pro 2019 15 inch ", 20180101, 16000, 20211201, 10000);
+                //Computer computer3 = new Computer("MacBook", "Pro 2020 15 inch ", 20180101, 18000, 20211201, 13000);
+
+
+                //dbContext.Computers.AddRange(computer2,computer3);
+                //Computer computer4 = new Computer("MacBook", "Pro 2020 15 inch ", 20180101, 18000, 20211201, 13000);
+                //dbContext.Add(computer4);
+
+                //dbContext.SaveChanges();
+
+
+
+
+
+                //(string brand, string modelName, int purchaseDate, int inicialCost, int expiredDate, int expiredCost)
                 // new Asset("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000),
-                 dbContext.Add(new Computer("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000));
-                  
-                 dbContext.SaveChanges();
-                
-                 var result = dbContext.Computers.ToList()
-                                   .Select(d => d.Brand);
-                Console.WriteLine(string.Join(", ", result));;
+                // dbContext.Add(new Computer("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000));
 
+                //// dbContext.SaveChanges();
 
-
-   //             Console.WriteLine("Hello World!");
-     //           using (var dbContext = new CustomTreatmentContext())
-       //         {
-         //           dbContext.Add(new Diet("Superduper diet", "Hard", 2, 200));
-           //         dbContext.SaveChanges();
-             //       var result = dbContext.Diets.ToList()
-               //         .Where(d => d.DietDaysProgram > 0)
-                 //       .Take(5)
-                   //     .OrderBy(d => d.DietDaysProgram)
- //                       .Select(d => d.DietName);
- //
-   //                 var resultSum = dbContext.Diets
-     //                   .Where(d => d.DietDaysProgram > 0)
-       //                 .OrderBy(d => d.DietDaysProgram)
-         //               .GroupBy(o => 1)
-           //             .Select(d =>
-             //               "Average days:" + (float)d.Sum(d => d.DietDaysProgram) / (float)Math.Max(1, d.Count()) +
-               //             "(total diets: " + d.Count() + ")"
-                 //       );
- //                   Console.WriteLine(string.Join(", ", result));
-   //                 Console.WriteLine(string.Join(", ", resultSum));
-     //           }
-
-
+                // var result = dbContext.Computers.ToList()
+                //                   .Select(d => d.Brand);
+                // Console.WriteLine(string.Join(", ", result));;
 
             }
         }
